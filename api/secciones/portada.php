c5a17fd93bfa10b157d54e7cfb75b13143e5e8e8
msb234 <91028899+msb234@users.noreply.github.com>
2025-01-09 20:20:59 +0100

    Update portada.php

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Portada",
+"titulo" => "Portada actualizada - msb234",
 "autor" => "mtorres",
 "resumen" => "El resumen maravilloso",
 ];
